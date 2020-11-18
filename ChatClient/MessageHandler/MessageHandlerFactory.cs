990e0f23331a1a9504880b6961bb49058d72daff
AS-2020 <armin_schmidt_hn@web.de>
2020-11-18 13:56:18 +0100

    Initial commit

---
+++ ChatClient/MessageHandler/MessageHandlerFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace ChatClient.MessageHandler
+{
+    public static class MessageHandlerFactory
+    {
+        public static IMessageHandler GetMessageHandler(int messageId)
+        {
+            switch (messageId)
+            {
+                case 1:
+                    return new ChatMessageHandler();
+                case 4:
+                    return new ConnectResponseMessageHandler();
+                case 5:
+                    return new NotificationHandler();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ChatClient/MessageHandler/NotificationHandler.cs
@@ -0,0 +1,19 @@
+﻿using ChatProtocol;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient.MessageHandler
+{
+    class NotificationHandler : IMessageHandler
+    {
+        public void Execute(TcpClient client, IMessage message)
+        {
+            ConnectNotification connectNotification = message as ConnectNotification;
+            Console.Write($"{connectNotification.Name}: ");
+
+        }
+    }
+}
+
